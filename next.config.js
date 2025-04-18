a3d1bcba3288099b39c83d4ff433d5b995343b31
MD Nazmul <nazmulhas36@gmail.com>
2025-04-18 22:10:15 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,  // Enable React strict mode to help identify potential problems
+  reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true,  // Ignore ESLint during build to speed up builds
+    ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['your-image-domain.com'], // Specify image domains for Next.js image optimization
-    unoptimized: true,  // Set to true for static exports
+    unoptimized: true,
   },
-  basePath: '/portfolio',  // Add this line
-  assetPrefix: '/portfolio/',  // Add this line
-  
   webpack(config, { isServer }) {
-    // Only optimize images for client-side builds
     if (!isServer) {
       config.module.rules.push({
         test: /\.svg$/,
